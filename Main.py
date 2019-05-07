6b7147317544b1bd78d723e7b34cc0c7f8e871e4
EvertonCa <eve.023@hotmail.com>
2019-05-07 00:37:05 -0300

    uncomment on main

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from Stitcher import Stitcher
 if __name__ == '__main__':
     handler = Handler()
     video_converter = VideoToFrames(handler)
-    # video_converter.video_in_frames()
+    video_converter.video_in_frames()
     stitcher = Stitcher(handler)
     stitcher.make_panoramic()
 
