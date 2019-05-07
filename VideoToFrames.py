86a3084d2eb5f2af4a995753aa3417abfae14b71
Lucas Laheras <lucaslaheras@hotmail.com>
2019-05-07 01:34:21 -0300

    choose FPS quantity to capture

--- VideoToFrames.py
+++ VideoToFrames.py
@@ -5,14 +5,30 @@ class VideoToFrames:
     def __init__(self, handler):
         self.handler = handler
 
-    def video_in_frames(self):
+    def videoInFramesThreads(self, fps):
+        step = int(1000 / fps)
+        firstFrame = 0
+        # TODO get amount of CPU
+        process = 4
         video_name = self.handler.find_video_name()
         vidcap = cv2.VideoCapture(self.handler.input_directory + video_name)
-        success, image = vidcap.read()
-        count = 1
-        while success:
-            image = cv2.resize(image, (768, 432))
-            cv2.imwrite(self.handler.temp_directory + "frame%d.jpg" % count, image)  # save frame as JPEG file
+        vidcap.set(cv2.CAP_PROP_POS_AVI_RATIO, 1)
+        lastFrame = vidcap.get(cv2.CAP_PROP_POS_MSEC)
+
+        Lim = [round(int(i * (int(lastFrame / 1000) - firstFrame) / process)) for i in range(process + 1)]
+        Lim = [round(i * 1000) for i in Lim]
+        print(Lim)
+        # TODO implement multiprocess
+        self.videoInFrames(Lim[0], Lim[1] - 1, step)
+        self.videoInFrames(Lim[1], Lim[2] - 1, step)
+        self.videoInFrames(Lim[2], Lim[3] - 1, step)
+        self.videoInFrames(Lim[3], Lim[4], step)
+
+    def videoInFrames(self, firstFrame, lastFrame, step):
+        video_name = self.handler.find_video_name()
+        vidcap = cv2.VideoCapture(self.handler.input_directory + video_name)
+        for i in range(firstFrame, lastFrame, step):
+            vidcap.set(cv2.CAP_PROP_POS_MSEC, i)
             success, image = vidcap.read()
-            print('Read a new frame: ', success)
-            count += 1
+            if success:
+                cv2.imwrite(self.handler.temp_directory + "frame%d.jpg" % i, image)
