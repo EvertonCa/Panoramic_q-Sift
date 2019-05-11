b7a289520d9b8478482c912fe013d0d7228b24d8
Lucas Laheras <lucaslaheras@hotmail.com>
2019-05-11 18:46:15 -0300

    put video in the middle of frames

--- Main.py
+++ Main.py
@@ -5,7 +5,11 @@ from Stitcher import Stitcher
 if __name__ == '__main__':
     handler = Handler()
     video_converter = VideoToFrames(handler)
-    video_converter.video_in_frames_threads(10)
+    #video_converter.video_in_frames_threads(10)
+    intervalo_menor = 4000
+    #intervalo_maior = 7000
+    #video_converter.remove_frames_sequentially(intervalo_menor, intervalo_maior)
+    video_converter.add_new_frames(intervalo_menor, 2)
     stitcher = Stitcher(handler)
     stitcher.make_panoramic()
 
--- VideoToFrames.py
+++ VideoToFrames.py
@@ -2,6 +2,7 @@ import cv2
 import datetime
 from multiprocessing import Pool
 from multiprocessing import cpu_count
+import os
 
 
 class VideoToFrames:
@@ -24,11 +25,31 @@ class VideoToFrames:
         with Pool(process) as p:
             p.map(self.video_in_frames, all_frames, process)
 
-    def video_in_frames(self, frame):
+    def video_in_frames(self, frame, begin=-1, indice=-1):
         video_name = self.handler.find_video_name()
         vidcap = cv2.VideoCapture(self.handler.input_directory + video_name)
         vidcap.set(cv2.CAP_PROP_POS_MSEC, frame)
         success, image = vidcap.read()
         image = cv2.resize(image, (768, 432))
-        if success:
+        if success and indice < 0:
             cv2.imwrite(self.handler.temp_directory + "frame%d.jpg" % frame, image)
+        elif success:
+            cv2.imwrite(self.handler.temp_directory + "frame%d.jpg" % (begin + indice), image)
+
+    def remove_frames_sequentially(self, first, last):
+        for i in range(first, last):
+            try:
+                os.remove(self.handler.temp_directory + "frame%d.jpg" % i)
+            except:
+                pass
+
+    def add_new_frames(self, begin, fps):
+        step = int(1000/fps)
+        video_name = self.handler.find_video_name()
+        vidcap = cv2.VideoCapture(self.handler.input_directory + video_name)
+        vidcap.set(cv2.CAP_PROP_POS_AVI_RATIO, 1)
+        last_frame = int(vidcap.get(cv2.CAP_PROP_POS_MSEC))
+
+        for i in range(0, last_frame, step):
+            #print(last_frame, step, int(i/step))
+            self.video_in_frames(i, begin, int(i/step))
